80ab6df37401bab14305f6c4debe274f5113dca4
jose vitor <josevitorrodrigues17@gmail.com>
2025-07-10 12:25:48 -0300

    update main.go

--- cmd/goCloud/main.go
+++ cmd/goCloud/main.go
@@ -4,15 +4,17 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 	"github.com/josevitorrodriguess/goCloud/internal/api"
 )
 
 func main() {
+	godotenv.Load()
 	api := api.Api{
 		Router: chi.NewMux(),
 	}
 
-	if err := http.ListenAndServe(":8080", api.Router); err != nil {
+	if err := http.ListenAndServe(":3000", api.Router); err != nil {
 		panic(err)
 	}
 }
