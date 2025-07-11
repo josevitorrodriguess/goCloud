81805f1c682dab7a243a5ac00c276001ca77a0b4
jose vitor <josevitorrodrigues17@gmail.com>
2025-07-10 23:52:38 -0300

    initializing api

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,7 +1,16 @@
 package api
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/josevitorrodriguess/goCloud/internal/auth"
+)
 
 type Api struct {
 	Router *chi.Mux
+}
+
+func (api *Api) Init() {
+	api.Router = chi.NewMux()
+	auth.NewAuth()
+	api.NewRouter()
 }
