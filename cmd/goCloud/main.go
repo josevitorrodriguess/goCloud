e2fcec3260a8a7452f78abaeafa8c01ae0393209
jose vitor <josevitorrodrigues17@gmail.com>
2025-07-15 15:52:45 -0300

    adding logs

--- cmd/goCloud/main.go
+++ cmd/goCloud/main.go
@@ -5,23 +5,30 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/josevitorrodriguess/goCloud/internal/api"
+	"github.com/josevitorrodriguess/goCloud/internal/logger"
 	"github.com/josevitorrodriguess/goCloud/internal/repository"
 	"github.com/josevitorrodriguess/goCloud/internal/storage/postgres"
 	"github.com/josevitorrodriguess/goCloud/internal/usecase"
 )
 
 func main() {
+	logger.Info("Starting goCloud server...")
+	
+	logger.Info("Loading environment variables")
 	godotenv.Load()
-	// Conecta ao banco e instancia o repositório e usecase
+	
+	logger.Info("Connecting to database")
 	db := postgres.ConnectDatabase()
 	repo := repository.NewUserRepository(db)
 	userUsecase := usecase.NewUserUsecase(*repo)
 
+	logger.Info("Initializing API")
 	apiInstance := api.Api{
 		UserUsecase: userUsecase,
 	}
 	apiInstance.Init()
 
+	logger.Info("Starting server on: http://localhost:3050")
 	if err := http.ListenAndServe(":3050", apiInstance.Router); err != nil {
 		panic(err)
 	}
