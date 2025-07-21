e06b2b298bbffefa661f0be38aa79c2d3cc35cb3
jose vitor <josevitorrodrigues17@gmail.com>
2025-07-21 14:12:25 -0300

    adding logs

--- internal/repository/file_repository.go
+++ internal/repository/file_repository.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/josevitorrodriguess/goCloud/internal/domain"
+	"github.com/josevitorrodriguess/goCloud/internal/logger"
 )
 
 type FileRepository struct {
@@ -21,6 +22,7 @@ func NewFileRepository(s3 *s3.Client, bucket string) *FileRepository {
 }
 
 func (fr *FileRepository) UploadFile(file domain.File) error {
+	logger.Info("Iniciando upload do arquivo: %s para o usuário: %s", file.Name, file.Email)
 	key := file.Email + "/" + file.Name
 	input := &s3.PutObjectInput{
 		Bucket: &fr.bucket,
@@ -28,10 +30,16 @@ func (fr *FileRepository) UploadFile(file domain.File) error {
 		Body:   bytes.NewReader(file.File),
 	}
 	_, err := fr.s3.PutObject(context.TODO(), input)
-	return err
+	if err != nil {
+		logger.Error("Erro ao fazer upload do arquivo: %v", err)
+		return err
+	}
+	logger.Info("Upload realizado com sucesso para o arquivo: %s", file.Name)
+	return nil
 }
 
 func (fr *FileRepository) GetFile(email, name string) ([]byte, error) {
+	logger.Info("Buscando arquivo: %s do usuário: %s", name, email)
 	key := email + "/" + name
 	input := &s3.GetObjectInput{
 		Bucket: &fr.bucket,
@@ -39,18 +47,22 @@ func (fr *FileRepository) GetFile(email, name string) ([]byte, error) {
 	}
 	result, err := fr.s3.GetObject(context.TODO(), input)
 	if err != nil {
+		logger.Error("Erro ao buscar arquivo: %v", err)
 		return nil, err
 	}
 	defer result.Body.Close()
 	buf := new(bytes.Buffer)
 	_, err = buf.ReadFrom(result.Body)
 	if err != nil {
+		logger.Error("Erro ao ler conteúdo do arquivo: %v", err)
 		return nil, err
 	}
+	logger.Info("Arquivo %s baixado com sucesso.", name)
 	return buf.Bytes(), nil
 }
 
 func (fr *FileRepository) GetAllFiles(email string) ([]string, error) {
+	logger.Info("Listando arquivos do usuário: %s", email)
 	prefix := email + "/"
 	input := &s3.ListObjectsV2Input{
 		Bucket: &fr.bucket,
@@ -58,24 +70,31 @@ func (fr *FileRepository) GetAllFiles(email string) ([]string, error) {
 	}
 	result, err := fr.s3.ListObjectsV2(context.TODO(), input)
 	if err != nil {
+		logger.Error("Erro ao listar arquivos: %v", err)
 		return nil, err
 	}
 	var files []string
 	for _, obj := range result.Contents {
-
 		if obj.Key != nil {
 			files = append(files, (*obj.Key)[len(prefix):])
 		}
 	}
+	logger.Info("Arquivos encontrados: %v", files)
 	return files, nil
 }
 
 func (fr *FileRepository) DeleteFile(email, name string) error {
+	logger.Info("Deletando arquivo: %s do usuário: %s", name, email)
 	key := email + "/" + name
 	input := &s3.DeleteObjectInput{
 		Bucket: &fr.bucket,
 		Key:    &key,
 	}
 	_, err := fr.s3.DeleteObject(context.TODO(), input)
-	return err
+	if err != nil {
+		logger.Error("Erro ao deletar arquivo: %v", err)
+		return err
+	}
+	logger.Info("Arquivo %s deletado com sucesso.", name)
+	return nil
 }
